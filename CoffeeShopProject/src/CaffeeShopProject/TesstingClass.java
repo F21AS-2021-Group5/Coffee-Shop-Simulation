66db994b5b633371b88f4adb03c4d2f5947120c8
karolinajudzentyte <karoliux.ju@gmail.com>
2021-02-03 13:23:11 +0000

    eddited tesssting class

--- CoffeeShopProject/src/CaffeeShopProject/TesstingClass.java
+++ CoffeeShopProject/src/CaffeeShopProject/TesstingClass.java
@@ -1,6 +1,16 @@
 package CaffeeShopProject;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class TesstingClass {
+	
+    private void readMenuItems(String fileName) {
+        BufferedReader br = null; //reader
+       
+    }
 	
 	public static void main(String[] args) {
 		Order item = new Order(null, null, null, 0, null);

796d8c3dd1c3c2bd25765a1fb034f19cdc0c8a66
karolinajudzentyte <karoliux.ju@gmail.com>
2021-02-03 11:02:22 +0000

    Created order class

--- CoffeeShopProject/src/CaffeeShopProject/Order.java
+++ CoffeeShopProject/src/CaffeeShopProject/Order.java
@@ -1,10 +1,46 @@
 package CaffeeShopProject;
 
-import java.io.*;
-import java.util.*;
-import java.util.concurrent.LinkedBlockingDeque;
 
 public class Order {
+	
+	private String name;
+	private String description;
+	private String category;
+	private String identifier;
+	private float cost;
+	
+	
+	public Order(String name, String description, String category, float cost, String identifier) {
+        this.name = name;
+        this.description = description;
+        this.category = category;
+        this.cost = cost;
+        this.identifier = identifier;
+    }
+	
+	// Generic get methods 
+	public String getName() {
+        return name;
+    }
+	
+	public String getDescription() {
+        return description;
+    }
+	
+	public String getCategory() {
+        return category;
+    }
+	
+	public String getIdentifier() {
+        return identifier;
+    }
+	
+	public float getCost() {
+		return cost;
+	}
+	
+	
+	
 	
 	
 

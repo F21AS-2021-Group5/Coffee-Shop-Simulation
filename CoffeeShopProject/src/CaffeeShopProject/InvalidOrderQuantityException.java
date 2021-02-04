28fe58153a5c1a35d7866495d59d7ce69f99e64f
valeriofranchi <v.franchi@hotmail.com>
2021-02-04 17:02:02 +0000

    corrected exception classes and added documentation to them

--- CoffeeShopProject/src/CaffeeShopProject/InvalidOrderQuantityException.java
+++ CoffeeShopProject/src/CaffeeShopProject/InvalidOrderQuantityException.java
@@ -1,8 +1,24 @@
+/**
+ * InvalidOrderQuantityException.java - class to implement an exception for invalid order quantity
+ * 
+ * @author Esther Rayssiguie 
+ * @author Jake Marrocco
+ * @author Karolina Judzentyte
+ * @author Valerio Franchi
+ * @version 0.1
+ * 
+ * Copyright (c) 2021 
+ * All rights reserved.
+ */
+
 package CaffeeShopProject;
 
 public class InvalidOrderQuantityException extends Exception{
 
+	/**
+	 * Constructor for InvalidOrderQuantityException class
+	 */
 	public InvalidOrderQuantityException() {
-		super("Invalid order ID.");
+		super("Invalid order quantity.");
 	}
 }
--- CoffeeShopProject/src/CaffeeShopProject/NoMatchingOrderIDException.java
+++ CoffeeShopProject/src/CaffeeShopProject/NoMatchingOrderIDException.java
@@ -1,7 +1,23 @@
+/**
+ * NoMatchingOrderIDException.java - class to implement an exception for no matching order IDs
+ * 
+ * @author Esther Rayssiguie 
+ * @author Jake Marrocco
+ * @author Karolina Judzentyte
+ * @author Valerio Franchi
+ * @version 0.1
+ * 
+ * Copyright (c) 2021 
+ * All rights reserved.
+ */
+
 package CaffeeShopProject;
 
 public class NoMatchingOrderIDException extends Exception{
 	
+	/**
+	 * Constructor of NoMatchingOrderIDException class
+	 */
 	public NoMatchingOrderIDException() {
 		super("No matching order ID.");
 	}

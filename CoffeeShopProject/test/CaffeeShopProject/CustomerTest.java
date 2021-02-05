eae4ff5b41ef0ae236505370eb43d526b01390b3
valeriofranchi <v.franchi@hotmail.com>
2021-02-05 02:07:02 +0000

    added test folder to path and customer test class with initialized test cases

---
+++ CoffeeShopProject/test/CaffeeShopProject/CustomerTest.java
@@ -0,0 +1,79 @@
+package CaffeeShopProject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class CustomerTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testCustomer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetTimestamp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetTimestamp() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOrderTotalPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetOrderTotalPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testReceipt() {
+		fail("Not yet implemented");
+	}
+
+}

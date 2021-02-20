cc7e71305db72e0400c1be4b2710cc2c3834bafa
teamBob19 <46927990+teamBob19@users.noreply.github.com>
2021-02-20 23:03:28 +0000

    Actions given to buttons and other. Added methods to Cashier class that I need

--- CoffeeShopProject/src/CaffeeShopProject/Cashier.java
+++ CoffeeShopProject/src/CaffeeShopProject/Cashier.java
@@ -3,5 +3,28 @@ package CaffeeShopProject;
 import java.time.LocalDateTime;
 
 public class Cashier {
+	
+	public float subtotal;
+	public float tax;
+	public float discount;
+	public float total;
+	
+	
+	// Those methods need to be completed!!
+	public float getCartSubtotalPrice() {
+		return subtotal;
+	}
+	
+	public float getCartTax() {
+		return tax;
+	}
+	
+	public float getDiscount() {
+		return discount;
+	}
+	
+	public float getCartTotalPrice() {
+		return total;
+	}
 
 }
--- CoffeeShopProject/src/CaffeeShopProject/GUI.java
+++ CoffeeShopProject/src/CaffeeShopProject/GUI.java
@@ -1,8 +1,22 @@
+/**
+ * Customer.java - class to implement a customer for the coffee shop simulation
+ * 
+ * @author Esther Rayssiguie 
+ * @author Jake Marrocco
+ * @author Karolina Judzentyte
+ * @author Valerio Franchi
+ * @version 0.1
+ * 
+ * Copyright (c) 2021 
+ * All rights reserved.
+ */
+
 package CaffeeShopProject;
 
 import java.util.HashMap;
 import java.util.Queue;
 import java.util.ArrayList;
+import java.time.LocalDateTime;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
@@ -16,6 +30,7 @@ import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.events.SelectionListener;
 
 public class GUI {
 	
@@ -30,12 +45,25 @@ public class GUI {
 	private Text txtCustomerId;
 	private List list_menu;
 	private ScrolledComposite scrolledComposite_Menu;
+	private List list_order;
+	private ScrolledComposite scrolledComposite_Order;
+	private List list_Receipt;
+	private ScrolledComposite scrolledComposite_Receipt;
+	private List list_Report;
+	private ScrolledComposite scrolledComposite_Report;
+	private Button btnAdd;
+	private Button btnRemove;
 	
+	private int itemQuantity;
 	ArrayList<String> customer_order = new ArrayList<String>();	//Order of current customer
+	private String[] itemSelected;
+	private String description;
+	
 	
 	Customer customer;
 	MenuItem menu_item;
 	CoffeeShop coffee;
+	Cashier cashier;
 
 	/**
 	 * Launch the application.
@@ -77,7 +105,7 @@ public class GUI {
 		
 		DisplayCustomer();
 		SelectCategory();
-		DisplayMenu();
+		//DisplayMenu();
 		DisplayItemDescription();
 		AddItem();
 		RemoveItem();
@@ -98,18 +126,49 @@ public class GUI {
 		btnNewCustomer.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
-				shell.close();		// CHANGE LATER, for now just close window
+				shell.redraw();		// Re-initialise shell
+				customer_order.removeAll(customer_order);	// Re-initialise current order
+				btnAdd.setEnabled(true);	// Add button enable again
+				btnRemove.setEnabled(true);	// Remove button again
+				list_menu.removeAll();	// empty list
+				list_order.removeAll();	// empty list
 			}
 		});
 		
 		// Display Current Customer and their ID
 		txtCustomerId = new Text(shell, SWT.BORDER);
-		txtCustomerId.setText("Customer ID" + customer.getId());
+		txtCustomerId.setText("Customer ID " + customer.getId());
 		txtCustomerId.setBounds(10, 55, 298, 26);
 	}
 	
 	// Select category and fill list of items from category chosen
 	private void SelectCategory() {
+		scrolledComposite_Menu = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite_Menu.setBounds(10, 122, 298, 206);
+		scrolledComposite_Menu.setExpandHorizontal(true);
+		scrolledComposite_Menu.setExpandVertical(true);
+		
+		list_menu = new List(scrolledComposite_Menu, SWT.BORDER | SWT.MULTI | SWT.V_SCROLL);
+		// When you select from list
+		list_menu.addSelectionListener(new SelectionListener() {
+			public void widgetSelected(SelectionEvent event) {
+				itemSelected = list_menu.getSelection();	// Add selected item to itemSelected list
+			}
+			public void widgetDefaultSelected(SelectionEvent event) {
+		        itemSelected = list_menu.getSelection();	// Add selected item to itemSelected list
+			}
+//			public void widgetDefaultSelected(SelectionEvent event) {
+//		        int[] selections = list_menu.getSelectionIndices();
+//		        String outText = "";
+//		        for (int loopIndex = 0; loopIndex < selections.length; loopIndex++)
+//		          outText += selections[loopIndex] + " ";
+//		        System.out.println("You selected: " + outText);
+//		      }
+		});
+		scrolledComposite_Menu.setContent(list_menu);
+		scrolledComposite_Menu.setMinSize(list_menu.computeSize(SWT.DEFAULT, SWT.DEFAULT));
+		
+		
 		Button btnMeal = new Button(shell, SWT.NONE);
 		btnMeal.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -177,31 +236,74 @@ public class GUI {
 		
 	}
 	
-	// Display Menu
-	private void DisplayMenu() {
-		scrolledComposite_Menu = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
-		scrolledComposite_Menu.setBounds(10, 122, 298, 206);
-		scrolledComposite_Menu.setExpandHorizontal(true);
-		scrolledComposite_Menu.setExpandVertical(true);
-		
-		list_menu = new List(scrolledComposite_Menu, SWT.BORDER);
-		scrolledComposite_Menu.setContent(list_menu);
-		scrolledComposite_Menu.setMinSize(list_menu.computeSize(SWT.DEFAULT, SWT.DEFAULT));
-	}
+//	// Display Menu
+//	private void DisplayMenu() {
+//
+//	}
 	
 	private void AddItem() {
 		// Quantity of the Item selected
 		Spinner spinner = new Spinner(shell, SWT.BORDER);
 		spinner.setBounds(91, 336, 59, 26);
+		itemQuantity = spinner.getIncrement();	// Get quantity of item selected
 		
 		// Add Item and its quantity desired
-		Button btnAdd = new Button(shell, SWT.NONE);
+		btnAdd = new Button(shell, SWT.NONE);
+		btnAdd.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				for(int i = 0; i< coffee.menu.size(); i ++) {
+					if(itemSelected.toString() == menu_item.getName()) {	// When item selected found, add Item
+						if(itemQuantity > 0) {	// if itemQuantity larger than 0
+							for (int j = 0; i < itemQuantity; i++)
+								customer_order.add(menu_item.getName());	//add item to order_list number of time it was selected
+							
+							LocalDateTime time = customer.timestamp;
+							try {
+								customer.addItem(menu_item.getIdentifier(), itemQuantity, time);	//add item & its quantity
+							} catch (InvalidMenuItemQuantityException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (InvalidMenuItemDataException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}	
+					}
+				}				
+			}
+		});
 		btnAdd.setBounds(168, 334, 51, 30);
 		btnAdd.setText("Add");
 	}
 	
+	// Remove Selected item from the list
 	private void RemoveItem() {
-		Button btnRemove = new Button(shell, SWT.NONE);
+		btnRemove = new Button(shell, SWT.NONE);
+		btnRemove.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				for(int i = 0; i< coffee.menu.size(); i ++) {
+					if(itemSelected.toString() == menu_item.getName()) {	// When item selected found, remove Item
+						try {
+							customer.removeItem(menu_item.getIdentifier(), itemQuantity);
+						} catch (NoMatchingMenuItemIDException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InvalidMenuItemQuantityException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InvalidMenuItemDataException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (InvalidCartItemException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+					}
+				}	
+			}
+		});
 		btnRemove.setBounds(10, 334, 66, 30);
 		btnRemove.setText("Remove");
 	}
@@ -211,6 +313,8 @@ public class GUI {
 		btnPurchase.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
+				btnAdd.setEnabled(false);	// Disable Add button
+				btnRemove.setEnabled(false);	// Disable Remove button
 			}
 		});
 		btnPurchase.setBounds(237, 334, 70, 30);
@@ -220,40 +324,56 @@ public class GUI {
 	// Display description of item chosen
 	private void DisplayItemDescription() {
 		txt_description = new Text(shell, SWT.BORDER);
-		txt_description.setText("Item Description" + menu_item.getDescription());
+		
+		//get the description of the item selected
+		for(int i = 0; i< coffee.menu.size(); i ++) {
+			if(itemSelected.toString() == menu_item.getName()) {
+				description = menu_item.getDescription();
+			}
+		}
+		txt_description.setText("Item Description " + description);
 		txt_description.setBounds(10, 378, 297, 60);
 	}
 	
 	private void DisplayPrice() {
 		txt_tax = new Text(shell, SWT.BORDER);
-		txt_tax.setText("Tax");
+		txt_tax.setText("Tax  " + cashier.getCartTax());
 		txt_tax.setBounds(10, 458, 190, 30);
 		
 		txt_subt = new Text(shell, SWT.BORDER);
-		txt_subt.setText("Subtotal");
+		txt_subt.setText("Subtotal  " + cashier.getCartSubtotalPrice());
 		txt_subt.setBounds(10, 513, 190, 30);
 		
 		txt_discnt = new Text(shell, SWT.BORDER);
-		txt_discnt.setText("Discount");
+		txt_discnt.setText("Discount  " + cashier.getDiscount());
 		txt_discnt.setBounds(10, 566, 190, 30);
 		
 		txt_total = new Text(shell, SWT.BORDER);
-		txt_total.setText("Total");
+		txt_total.setText("Total  " + customer.getCartTotalPrice());
 		txt_total.setBounds(10, 613, 190, 30);
 	}
 	
 	// Display List of Current Order from Current Customer
 	private void DisplayOrder() {
 		Button btnOrderList = new Button(shell, SWT.NONE);
+		btnOrderList.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				list_order.removeAll();	// empty list
+				for(int i = 0; i<customer_order.size(); i++) {
+					list_order.add(customer_order.toString());	// Show Order list
+				}			
+			}
+		});
 		btnOrderList.setBounds(217, 456, 90, 30);
 		btnOrderList.setText("Order List");
 		
-		ScrolledComposite scrolledComposite_Order = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite_Order = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite_Order.setBounds(360, 10, 177, 245);
 		scrolledComposite_Order.setExpandHorizontal(true);
 		scrolledComposite_Order.setExpandVertical(true);
 		
-		List list_order = new List(scrolledComposite_Order, SWT.BORDER);
+		list_order = new List(scrolledComposite_Order, SWT.BORDER);
 		scrolledComposite_Order.setContent(list_order);
 		scrolledComposite_Order.setMinSize(list_order.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 		
@@ -262,15 +382,21 @@ public class GUI {
 	// Display Receipt of Current Customer
 	private void DisplayReceipt() {
 		Button btnReceipt = new Button(shell, SWT.NONE);
+		btnReceipt.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				list_Receipt.add(customer.receipt());	// Display receipt		
+			}
+		});
 		btnReceipt.setBounds(217, 511, 90, 30);
 		btnReceipt.setText("Receipt");
 		
-		ScrolledComposite scrolledComposite_Receipt = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite_Receipt = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite_Receipt.setExpandVertical(true);
 		scrolledComposite_Receipt.setExpandHorizontal(true);
 		scrolledComposite_Receipt.setBounds(360, 277, 177, 366);
 		
-		List list_Receipt = new List(scrolledComposite_Receipt, SWT.BORDER);
+		list_Receipt = new List(scrolledComposite_Receipt, SWT.BORDER);
 		scrolledComposite_Receipt.setContent(list_Receipt);
 		scrolledComposite_Receipt.setMinSize(list_Receipt.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 		
@@ -282,24 +408,24 @@ public class GUI {
 		btnFinalReportCoffee.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent arg0) {
+				//list_Report.add(coffee.); SHOW FINAL REPORT
 			}
 		});
 		btnFinalReportCoffee.setBounds(217, 613, 90, 30);
 		btnFinalReportCoffee.setText("Final Report");
 		
-		ScrolledComposite scrolledComposite_Report = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
+		scrolledComposite_Report = new ScrolledComposite(shell, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL);
 		scrolledComposite_Report.setExpandVertical(true);
 		scrolledComposite_Report.setExpandHorizontal(true);
 		scrolledComposite_Report.setBounds(559, 10, 213, 633);
 		
-		List list_Report = new List(scrolledComposite_Report, SWT.BORDER);
+		list_Report = new List(scrolledComposite_Report, SWT.BORDER);
 		scrolledComposite_Report.setContent(list_Report);
 		scrolledComposite_Report.setMinSize(list_Report.computeSize(SWT.DEFAULT, SWT.DEFAULT));
 	}
 	
 	public void update() {
 		SelectCategory();
-		DisplayMenu();
 		DisplayOrder();	
 		DisplayReceipt();
 		DisplayFinalReport();

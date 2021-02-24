c33972521453bf95ab5781f4e9bb61eaf1166b4d
Jake Marrocco <48798357+AndreaLeadsom@users.noreply.github.com>
2021-02-24 17:54:58 +0000

    Update to Cashier Class

--- CoffeeShopProject/src/CaffeeShopProject/Cashier.java
+++ CoffeeShopProject/src/CaffeeShopProject/Cashier.java
@@ -1,6 +1,8 @@
 package CaffeeShopProject;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Cashier {
 	
@@ -8,22 +10,45 @@ public class Cashier {
 	public float tax;
 	public float discount;
 	public float total;
-
+	HashMap<String, ArrayList<LocalDateTime>> cart;
+	Customer customer;
 	
 	// Those methods need to be completed!!
 	public float getCartSubtotalPrice() {
+		subtotal = customer.getCartTotalPrice();
 		return subtotal;
 	}
 	
 	public float getCartTax() {
+		// get  cart from Customer class
+		cart = customer.getCart();
+		// loop through items in cart 
+		// if item is taxable, add 20% of item price to tax
+		if (cart.containsKey("DRINK001")) {
+			
+		} 
+		tax = getCartSubtotalPrice()/5;
 		return tax;
 	}
 	
 	public float getDiscount() {
+		// 3 drinks & 1 food = 20% off //
+		if (cart.containsKey("DRINK001") && cart.containsKey("FOOD001")) {
+			discount = getCartSubtotalPrice()/5;
+		}
+		// 2 drinks & 2 pastry = 25% off //
+		if (cart.containsKey("DRINK001") && cart.containsKey("PASTRY001")) {
+			discount = getCartSubtotalPrice()/4;
+		}
+		// 1 drink & 1 food & 1 pastry = £6 //
+		if (cart.containsKey("DRINK001") && cart.containsKey("FOOD001") && cart.containsKey("PASTRY001")) {
+			discount = getCartSubtotalPrice() - 6;
+		}
 		return discount;
 	}
 	
 	public float getCartTotalPrice() {
+		total = subtotal + tax - discount;
 		return total;
 	}
 

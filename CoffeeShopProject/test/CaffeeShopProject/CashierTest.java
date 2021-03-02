76709003534eb85d3e3b345e701b9934018aa628
Jake Marrocco <jm98@hw.ac.uk>
2021-03-02 20:10:41 +0000

    Cashier Test Class

---
+++ CoffeeShopProject/test/CaffeeShopProject/CashierTest.java
@@ -0,0 +1,224 @@
+/**
+ * CashierTest.java - JUnit Test Class for the Cashier class
+ * 
+ * @author Esther Rayssiguie 
+ * @author Jake Marrocco
+ * @author Karolina Judzentyte
+ * @author Valerio Franchi
+ * @version 0.1
+ * 
+ * Copyright (c) 2021 
+ * All rights reserved.
+ */
+
+
+package CaffeeShopProject;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.Test;
+
+import java.time.DateTimeException;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+
+import CaffeeShopProject.Customer;
+import CaffeeShopProject.Cashier;
+import CaffeeShopProject.CoffeeShop;
+
+
+
+class CashierTest {
+	
+	private static Cashier cashier;
+	private Customer customer;
+	private static CoffeeShop shop;
+	private double delta = 0.001;
+
+	@BeforeAll
+	static void setUp() throws Exception {
+		
+		System.out.println("Begin Cashier Test");
+		
+		//initialize main class
+		shop = new CoffeeShop("MenuItems");	
+	}
+	
+	@AfterEach
+	void tearDown() throws Exception {
+		//System.out.println("new test");
+	}
+	
+
+	
+	
+	@Test
+	void testcreateNewCustomer() {
+		// create new customer //
+		LocalDateTime t = LocalDateTime.now();
+		Customer customer = new Customer("Andrew", "A1", t);
+		
+		// check for customer information //
+		assertTrue(customer.name == "Andrew");
+		assertTrue(customer.id == "A1");
+		assertTrue(customer.timestamp == t);
+		
+	}
+	
+	@Test
+	void testcreateNewCashier() {
+		LocalDateTime t = LocalDateTime.now();
+		Customer customer = new Customer("Andrew", "A1", t);
+		Cashier cashier = new Cashier("Beth");
+		cashier.createNewCustomer(customer.name);
+	
+		assertTrue(cashier.ID == "Beth");
+		
+	}
+		
+	@Test
+	void testgetCartTax() {
+		
+		LocalDateTime t = LocalDateTime.now();
+		Customer customer = new Customer("Andrew", "A1", t);
+		
+		Cashier cashier = new Cashier("B");
+		cashier.createNewCustomer(customer.name);
+
+		
+		// test for zero price //
+		cashier.subtotal = 0;
+		assertEquals(0, cashier.getCartTax(), delta);
+		
+		// test for intiger price //
+		cashier.subtotal = 4;
+		assertEquals(4*0.25, cashier.getCartTax(), delta);
+		
+		// test for decimal tax //
+		cashier.subtotal = (float) 2.00;
+		assertEquals(2*0.25, cashier.getCartTax(), delta);
+		
+		// test for decimal price //
+		cashier.subtotal = (float) 2.99;
+		assertEquals(2.99*0.25, cashier.getCartTax(), delta);
+		
+
+	}
+	
+	@Test
+	void testgetDiscount1() throws InvalidMenuItemQuantityException, InvalidMenuItemDataException {
+			
+		LocalDateTime t = LocalDateTime.now();
+		Customer customer = new Customer("Andrew", "A1", t);
+		
+		Cashier cashier = new Cashier("B");
+		cashier.createNewCustomer(customer.name);
+		cashier.currentCustomer = customer;
+		
+		String itemId1 = "Cappuccino";
+		String itemId2 = "Panini";
+		String itemId3 = "Muffin";
+				
+		// test discount 1 //
+		customer.addItem(itemId1, 1, LocalDateTime.now());
+		customer.addItem(itemId2, 1, LocalDateTime.now());
+		customer.addItem(itemId3, 1, LocalDateTime.now());
+
+		assertTrue(customer.cart.containsKey(itemId1));
+		assertTrue(customer.cart.containsKey(itemId2));
+		assertTrue(customer.cart.containsKey(itemId3));
+
+		assertEquals(8.2, cashier.getCartSubtotalPrice(), delta);
+		assertEquals(8.2 - 5, cashier.getDiscount(), delta);
+
+		assertTrue(cashier.discount1 == 1);
+		assertTrue(cashier.discount2 == 0);
+		assertTrue(cashier.discount3 == 0);
+		
+	}
+	
+	@Test
+	void testgetDiscount2() throws InvalidMenuItemQuantityException, InvalidMenuItemDataException {
+
+		LocalDateTime t = LocalDateTime.now();
+		Customer customer = new Customer("Andrew", "A1", t);
+		
+		Cashier cashier = new Cashier("B");
+		cashier.createNewCustomer(customer.name);
+		cashier.currentCustomer = customer;
+		
+		String itemId1 = "Cappuccino";
+		String itemId2 = "Panini";
+		String itemId3 = "Muffin";
+
+		customer.addItem(itemId1, 3, LocalDateTime.now());
+		customer.addItem(itemId2, 1, LocalDateTime.now());
+		
+		assertTrue(customer.cart.containsKey(itemId1));
+		assertTrue(customer.cart.containsKey(itemId2));
+
+		// test discount 2 //
+		float price = cashier.getCartSubtotalPrice();
+		assertEquals(price*0.2,cashier.getDiscount(), delta);
+
+		assertTrue(cashier.discount1 == 0);
+		assertTrue(cashier.discount2 == 1);
+		assertTrue(cashier.discount3 == 0);
+	}
+	
+	@Test
+	void testgetDiscount3() throws InvalidMenuItemQuantityException, InvalidMenuItemDataException {
+		
+
+		LocalDateTime t = LocalDateTime.now();
+		Customer customer = new Customer("Andrew", "A1", t);
+		
+		Cashier cashier = new Cashier("B");
+		cashier.createNewCustomer(customer.name);
+		cashier.currentCustomer = customer;
+		
+		String itemId1 = "Cappuccino";
+		String itemId2 = "Panini";
+		String itemId3 = "Muffin";
+
+		customer.addItem(itemId3, 3, LocalDateTime.now());
+		
+		// test discount 3 //
+		float price = cashier.getCartSubtotalPrice();
+		assertEquals(price*0.25,cashier.getDiscount(), delta);
+
+		assertTrue(cashier.discount1 == 0);
+		assertTrue(cashier.discount2 == 0);
+		assertTrue(cashier.discount3 == 1);
+	}
+	
+	@Test
+	void testgetCartTotalPrice() throws InvalidMenuItemQuantityException, InvalidMenuItemDataException {
+		System.out.println("----------- now -----------");
+		
+		LocalDateTime t = LocalDateTime.now();
+		Customer customer = new Customer("Andrew", "A1", t);
+		
+		Cashier cashier = new Cashier("B");
+		cashier.createNewCustomer(customer.name);
+		cashier.currentCustomer = customer;
+		
+		String itemId1 = "Cappuccino";
+		String itemId2 = "Panini";
+		String itemId3 = "Muffin";
+		
+		customer.addItem(itemId1, 2, LocalDateTime.now());
+		customer.addItem(itemId2, 1, LocalDateTime.now());
+		customer.addItem(itemId3, 4, LocalDateTime.now());
+		
+		System.out.println(cashier.getCartSubtotalPrice());
+
+		assertEquals(2.6*2+3.25*1+2.35*4-cashier.getDiscount()+cashier.getCartTax(),cashier.getCartTotalPrice(), delta);
+	}
+}

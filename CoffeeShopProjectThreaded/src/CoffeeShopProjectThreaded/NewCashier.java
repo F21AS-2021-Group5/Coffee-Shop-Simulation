0dab5ff3ae79554840ec4a0ad2a52c46961d97f0
teamBob19 <46927990+teamBob19@users.noreply.github.com>
2021-03-13 15:03:05 +0000

    New Cashier class for threading

---
+++ CoffeeShopProjectThreaded/src/CoffeeShopProjectThreaded/NewCashier.java
@@ -0,0 +1,28 @@
+/**
+ * NewCashier.java - cashier class, each cashier is a thread dealing with customers orders 
+ * 
+ * @author Esther Rayssiguie 
+ * @author Jake Marrocco
+ * @author Karolina Judzentyte
+ * @author Valerio Franchi
+ * @version 0.1
+ * 
+ * Copyright (c) 2021 
+ * All rights reserved.
+ */
+
+package CoffeeShopProjectThreaded;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Queue;
+
+
+public class NewCashier extends Thread{
+	
+	public String ID;	//Cashier ID
+	public Customer currentCustomer;
+	private Queue<Customer> queue;
+	private NewGUI GUI;
+
+}

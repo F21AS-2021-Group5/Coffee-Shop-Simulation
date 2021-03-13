a7cc8a283a77a354faffa75e4b53ed65dc3c7f46
teamBob19 <46927990+teamBob19@users.noreply.github.com>
2021-03-13 21:26:24 +0000

    Updated Cashier. Added in Log class

---
+++ CoffeeShopProjectThreaded/src/CoffeeShopProjectThreaded/Log.java
@@ -0,0 +1,38 @@
+/**
+ * Log.java - class to update and get log 
+ * 
+ * @author Esther Rayssiguie 
+ * @author Jake Marrocco
+ * @author Karolina Judzentyte
+ * @author Valerio Franchi
+ * @version 0.1
+ * 
+ * Copyright (c) 2021 
+ * All rights reserved.
+ */
+
+package CoffeeShopProjectThreaded;
+
+public class Log {
+	
+	private static Log instance = new Log();
+	private String log;
+	
+	private void Log() {
+		log ="Coffee Shop Log\n\n"; 
+	}
+	
+	private static Log getInstance() {
+		return instance;
+	}
+	
+	public void updateLog(String info) {
+		log += info + "\n";
+		System.out.println(info);
+	}
+	
+	public String getLog() {
+		return log;
+	}
+
+}
--- CoffeeShopProjectThreaded/src/CoffeeShopProjectThreaded/NewCashier.java
+++ CoffeeShopProjectThreaded/src/CoffeeShopProjectThreaded/NewCashier.java
@@ -21,16 +21,58 @@ import java.util.Queue;
 public class NewCashier implements Runnable{
 	
 	public String ID;	//Cashier ID
+	public String name; //Cashier name
 	public Customer currentCustomer;
 	private Queue<Customer> queue;
 	private NewGUI GUI;
+	private static int cashierCount = 0;
+	public int cashierNumber = 0;
+	
+	private Log log;
+	
+	public NewCashier(String name, String id, Queue<Customer> queue, NewGUI gui) {
+		this.ID = id;
+		this.name = name;
+		this.setQueue(queue);
+		this.GUI = gui;
+		cashierNumber = cashierCount++;
+	}
 	
 	
 	
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-		
+		while(true) {
+			while(!(queue.isEmpty())) {
+				System.out.println("Currently waiting in queue " + queue.size());
+				if(!(queue.isEmpty())) {
+					customers();
+				}
+				try {
+					Thread.sleep(100);
+				}catch(InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public synchronized void customers() {
+		//Customer who purchased their order is removed form the queue
+		currentCustomer = queue.remove();
+		log.updateLog(name + " " + ID + ": Is processing order of  " + currentCustomer.getName());
+		//String orderID = currentCustomer.get Order ID
+		double price = currentCustomer.cartTotalPrice;
+		log.updateLog(name + " " + ID + ": Processed the oder of " + currentCustomer.getName() + " Total amount is: " + price + "£"); 
+	}
+	
+	public void setQueue(Queue<Customer> queue) {
+		this.queue = queue;
+	}
+	
+	public Queue<Customer> getQueue(){
+		return queue;
 	}
 
 }

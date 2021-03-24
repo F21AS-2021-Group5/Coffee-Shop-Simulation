6a1e070f1449479cdbe4e16f2e45fc9ee951bb20
Jake Marrocco <jm98@hw.ac.uk>
2021-03-24 22:59:03 +0000

    GUI update

--- CoffeeShopProjectThreaded/src/CoffeeShopProjectThreaded/NewGUI.java
+++ CoffeeShopProjectThreaded/src/CoffeeShopProjectThreaded/NewGUI.java
@@ -16,6 +16,7 @@ package CoffeeShopProjectThreaded;
 //import org.omg.PortableServer.THREAD_POLICY_ID;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -32,14 +33,20 @@ public class NewGUI extends Thread{
 	int height = 600;
 	
 	JFrame frame;
-	Customer currentCustomer;
+	private static Customer currentCustomer;
 	JTextArea customerlist = new JTextArea("Customer List");
+	JTextArea onlineCustomerlist = new JTextArea("Online Customer List");
 	JTextArea orders = new JTextArea("Order List");
+	JTextArea temp = new JTextArea("temp");
 	JPanel cashier = new JPanel();
 	
 	CoffeeShop main;
+	CustomerQueue customerQueue;
 	private Queue<Customer> queue;
 	
+	String CustomerName;
+	String Items;
+	int queueSize;
 	//GUI constructor
 	public NewGUI(CoffeeShop main) {
 		this.main = main;
@@ -54,42 +61,84 @@ public class NewGUI extends Thread{
 		frame.setVisible(true);
 		//Close when Exit
 		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		
-		frame.repaint();
 	}
+	
+
 	
 	public synchronized void DisplayCustomers() {
 		//Clear customer list
 		customerlist.removeAll();
-		
-		//Size is 1/4 of the width of the frame and same height
-		int customerListWidth = width;
+		//Size is 1/4 of the height of the frame and half the height
+		int customerListWidth = width/2;
 		int customerListHeight = height / 4;
-		customerlist.setBounds(0, 0, customerListWidth, customerListHeight);
+		customerlist.setBounds(10, 0, customerListWidth, customerListHeight);
 		
 		
 		
+		frame.getContentPane().add(customerlist);
+		customerlist.setText("queue:");
+		customerlist.append("\n Name:  ");
 	}
 	
 	public synchronized void DisplayOnlineCustomers() {
+		onlineCustomerlist.removeAll();
 		
+		int onlineCustomerListWidth = width/2;
+		int onlineCustomerListHeight = height / 4;
+		onlineCustomerlist.setBounds(width/2, 0, onlineCustomerListWidth, onlineCustomerListHeight);
+		
+		frame.getContentPane().add(onlineCustomerlist);
+		onlineCustomerlist.setText("online queue:");
 	}
 	
 	public void DisplayCashiers() {
+		int cashierWidth = width/2;
+		int cashierHeight = height / 4;
+		customerlist.setBounds(0, cashierHeight*2, cashierWidth, cashierHeight);
 		
+		
+		CustomerName = main.cashier.currentCustomer.getName();
+		Items = main.cashier.currentCustomer.cart.toString();
+		
+		System.out.println(main.cashier.currentCustomer.getCartTotalPrice());
+		
+		frame.getContentPane().add(customerlist);
+		customerlist.setText("Current Customer:");
+		customerlist.append("\n Name:  ");
+		customerlist.append(CustomerName);
+		customerlist.append("\n Items:  ");
+		for (String key : main.cashier.currentCustomer.cart.keySet()) {
+            customerlist.append(key + "\n");
+            //System.out.println(key);
+        }
+		customerlist.append("\n Total Price:  ");
+		customerlist.append(String.valueOf(main.cashier.currentCustomer.getCartTotalPrice()));
 	}
 	
 	public void DisplayOrders() {
 		int orderWidth = width;
 		int orderHeight = height/4;
-		orders.setBounds(0, 2 * height / 4, orderWidth, orderHeight);
+		orders.setBounds(0, height/4, orderWidth, orderHeight);
 		//if cashier has a customer (currentCustomer not null)
 		// Orders cashierDisp = get Cashier ID handling current customer
 		// orders.setText 
+		//frame.getContentPane().add(orders);
+		orders.setText("orders:");
+		//orders.append("orders:");
 		orders.setVisible(true);
 		frame.add(orders);
 		
 	}
+	
+	public synchronized void DisplayTemp() {		
+		int onlineCustomerListWidth = width;
+		int onlineCustomerListHeight = height / 4;
+		temp.setBounds(width, height, onlineCustomerListWidth, onlineCustomerListHeight);
+		
+		frame.getContentPane().add(temp);
+		temp.setText("queue:");
+	}
+	
 	
 	void CreateCashierDisplay(Cashier cash, int totalCashier, int currentCashier) {
 		int cashierWidth = width / totalCashier;
@@ -110,6 +159,7 @@ public class NewGUI extends Thread{
 		DisplayOnlineCustomers();
 		DisplayCashiers();
 		DisplayOrders();
+		DisplayTemp();
 		
 		frame.repaint();
 	}
